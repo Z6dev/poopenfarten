e0743d9c9f54b5eb3483e4642f61cfe487c4b8ff
Z6dev <168093172+Z6dev@users.noreply.github.com>
2025-11-12 09:18:40 +0700

    Update pf_vec.h

--- pf_vec.h
+++ pf_vec.h
@@ -7,50 +7,57 @@
 // Single header vector library made by Z6dev
 // The Unlicense, please do not credit me.
 // Who would use it anyway?
+// Define PF_VEC_IMPLEMENTATION in only ONE source file to use this library
 
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 
-// Use this bruh HEHE BOI
-#define pf_vec_t(T)                                                                                \
-	struct {                                                                                       \
-		T *data;                                                                                   \
-		size_t size;                                                                               \
-		size_t capacity;                                                                           \
-	}
+typedef struct {
+	size_t capacity;
+	size_t size;
+} _pf__vec_header;
 
-// Syntactic Sugar
-#define pf_vec(T) pf_vec_t(T)
+#define _pf_getVectorHeader(vector) ((_pf__vec_header*)((char*)(vector) - sizeof(_pf__vec_header)))
 
-// Implementation
-//------------------------------------------------------------
+#define pf_vectorSize(vector) ((vector) ? _pf_getVectorHeader(vector)->size : 0)
+#define pf_vectorCapacity(vector) ((vector) ? _pf_getVectorHeader(vector)->capacity : 0)
 
-#define pf_vec_init()                                                                              \
-	{ NULL, 0, 0 }
+// Create a new vector
+#define pf_vectorCreate(T, cap) \
+	((T*)vec__create(sizeof(T), cap))
 
-#define pf_vec_push(vec, value)                                                                    \
-	do {                                                                                           \
-		if ((vec).size == (vec).capacity) {                                                        \
-			(vec).capacity = (vec).capacity ? (vec).capacity * 2 : 4;                              \
-			(vec).data = realloc((vec).data, (vec).capacity * sizeof(*(vec).data));                \
-			if (!(vec).data) {                                                                     \
-				fprintf(stderr, "Out of memory in pf_vec_push!\n");                                \
-				exit(1);                                                                           \
-			}                                                                                      \
-		}                                                                                          \
-		(vec).data[(vec).size++] = (value);                                                        \
+// Push a new element
+#define pf_vectorPush(vector, value)                             \
+	do {                                                         \
+		if (pf_vectorSize(vector) == pf_vectorCapacity(vector))  \
+			(vector) = vec__grow(vector, sizeof(*(vector)));     \
+		(vector)[_pf_getVectorHeader(vector)->size++] = (value); \
 	} while (0)
 
-#define pf_vec_free(vec)                                                                           \
-	do {                                                                                           \
-		free((vec).data);                                                                          \
-		(vec).data = NULL;                                                                         \
-		(vec).size = (vec).capacity = 0;                                                           \
+// Free vector
+#define pf_vectorFree(vector)                          \
+	do {                                               \
+		if (vector) free(_pf_getVectorHeader(vector)); \
+		vector = NULL;                                 \
 	} while (0)
 
-#define pf_vec_get(vec, i) ((vec).data[(i)])
-#define pf_vec_len(vec) ((vec).size)
-#define pf_vec_cap(vec) ((vec).capacity)
+
+#ifdef PF_VECTOR_IMPLEMENTATION
+
+// Internal helpers
+static void* _pf__vec__create(size_t elem_size, size_t capacity) {
+	_pf__vec_header* h = malloc(sizeof(_pf__vec_header) + elem_size * capacity);
+	h->size = 0;
+	h->capacity = capacity;
+	return (void*)(h + 1);
+}
+
+static void* _pf__vec__grow(void* vector, size_t elem_size) {
+	size_t new_cap = pf_vectorCapacity(vector) ? pf_vectorCapacity(vector) * 2 : 4;
+	_pf__vec_header* old_h = _pf_getVectorHeader(vector);
+	_pf__vec_header* new_h = realloc(old_h, sizeof(_pf__vec_header) + elem_size * new_cap);
+	new_h->capacity = new_cap;
+	return (void*)(new_h + 1);
+}
+#endif // PF_VECTOR_IMPLEMENTATION
 
 #endif // PF_VEC_H
